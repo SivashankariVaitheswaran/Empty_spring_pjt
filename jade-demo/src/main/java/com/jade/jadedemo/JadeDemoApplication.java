cbb8adbaef74fe8d3f60b7ae2220ab233f561c84
SivashankariVaitheswaran <sivashankari@brillersys.com>
2019-12-17 08:46:39 +0530

    text

---
+++ jade-demo/src/main/java/com/jade/jadedemo/JadeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.jade.jadedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JadeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JadeDemoApplication.class, args);
+	}
+
+}
